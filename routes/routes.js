fa291626579b75248ca2422576bbb75f6ae62840
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2021-03-02 12:11:56 -0600

    fixing range #3

--- routes/routes.js
+++ routes/routes.js
@@ -59,9 +59,8 @@ router.post("/api/workouts", (req, res) => {
 });
 
 router.get("/api/workouts/range", (req, res) => {
-    db.Workout.aggregate([{
-            
-        $limit: 7,
+    db.Workout.aggregate([{ $limit: 7 },
+        {
         $addFields: {
             totalDuration: {
                 $sum: "$exercises.duration"
