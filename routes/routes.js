579268ae1d37586dd6eea20ef0999b31785d0f32
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2021-03-02 00:37:32 -0600

    working on api routes

--- routes/routes.js
+++ routes/routes.js
@@ -15,21 +15,50 @@ router.get("/stats", (req, res) => {
 });
 
 //api routes
-router.get("/api/workout", (req, res) => {
-    db.find({})
-    .sort({ date:-1 })
+router.get("/api/workouts", (req, res) => {
+    db.Workout.aggregate([{
+        $addFields: {
+            "totalDuration": {
+                $sum: "$exercise.duration"
+            },
+            "duration": "exercise.duration"
+        }
+    }]    )
     .then(dbWorkout => {
         res.json(dbWorkout);
     })
+    .catch(error => {
+        res.status(401).json(error);
+    });
 });
 
-router.post("/api/workout", ({body}, res) => {
-    db.create(body)
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.find({_id: req.params.id},
+        {
+            $push: { Exercise: req.body },
+        })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(error => {
+            res.status(401).json(error);
+        });
+});
+
+router.post("/api/workouts", (req, res) => {
+    db.Workout.create({})
     .then(dbWorkout => {
         res.json(dbWorkout);
     })
     .catch(error => {
         res.status(400).json(error);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
     });
 });
 
